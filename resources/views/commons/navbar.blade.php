9d9d08b120865716ff3f5ad83c332476939df6c2
mio.mio <omochi@techacademy.jp>
2018-06-11 01:52:33 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -12,6 +12,10 @@
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
+                       <ul class="nav navbar-nav navbar-right">
+                            <li>{!! link_to_route('signup.get', 'Signup') !!}</li>
+                            <li><a href="#">Login</a></li>
+                        </ul>
                     <li>{!! link_to_route('Tasks.create', '新規タスクの投稿') !!}</li>
                 </ul>
             </div>
